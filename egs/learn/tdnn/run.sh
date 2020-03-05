503acd05c6a400bf489d3257ac01300fd410136c
Ban <sawpara@126.com>
2020-03-05 19:09:22 +0800

    update

--- egs/learn/tdnn/run.sh
+++ egs/learn/tdnn/run.sh
@@ -6,4 +6,16 @@
 # created time : Thu 05 Mar 2020 06:35:49 PM CST
 ###############################################################################
 
-. cmd.sh
+. ./cmd.sh
+. ./path.sh
+
+H=$(pwd)
+
+thchs=/home/aban/data/thchs30
+
+stage=1
+
+if [ $stage -le 1 ]; then
+    local/thchs-30_data_prep.sh $H $thchs/data_thchs30 || exit 1;
+fi
+
