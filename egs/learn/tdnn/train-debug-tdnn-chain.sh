fa49115d5dbd18c0ae08765a0955670981ae78ca
Ban <sawpara@126.com>
2020-03-10 17:42:55 +0800

    update

--- egs/learn/tdnn/train-debug-tdnn-chain.sh
+++ egs/learn/tdnn/train-debug-tdnn-chain.sh
@@ -8,37 +8,12 @@
 
 . path.sh
 
-num_debug=1
+num_debug=100
 
-feats=./trash/feats.scp
-cmvns=./trash/cmvn.scp
-input=./trash/applied_cmvn.ark
+egs_org=./exp/tri8b/egs/cegs.1.ark
+egs_new=./trash/cegs.debug.ark
 
-head -n $num_debug data/mfcc/test/split10/1/feats.scp > $feats
-head -n $num_debug data/mfcc/test/split10/1/cmvn.scp > $cmvns
+nnet3-chain-subset-egs --n=$num_debug ark:$egs_org ark:$egs_new
 
-apply-cmvn \
-    --norm-means=true --norm-vars=true \
-    --utt2spk=ark:data/mfcc/test/split10/1/utt2spk \
-    scp:$cmvns \
-    scp:$feats ark:$input
 
-nnet3-latgen-faster\
-    --frame-subsampling-factor=3 \
-    --frames-per-chunk=140 \
-    --extra-left-context=0  \
-    --extra-right-context=0 \
-    --extra-left-context-initial=0 \
-    --extra-right-context-final=0 \
-    --minimize=false --max-active=7000 --min-active=200 \
-    --beam=15.0 --lattice-beam=8.0 --acoustic-scale=1.0 \
-    --allow-partial=true \
-    --word-symbol-table=/home/aban/devel/aban-kaldi/egs/learn/tdnn/exp/tri7b_tree/graph/words.txt \
-    /home/aban/devel/aban-kaldi/egs/learn/tdnn/exp/tri8b/final.mdl \
-    /home/aban/devel/aban-kaldi/egs/learn/tdnn/exp/tri7b_tree/graph/HCLG.fst \
-    ark:$input \
-    ark:./trash/lattice.ark
 
-#'ark:|lattice-scale --acoustic-scale=10.0 ark:- ark:- | gzip -c >/home/aban/devel/aban-kaldi/egs/learn/tdnn/exp/tri8b/decode_lang/lat.1.gz' 
-
-echo nnet3-latgen-faster --frame-subsampling-factor=3 --frames-per-chunk=140 --extra-left-context=0 --extra-right-context=0 --extra-left-context-initial=0 --extra-right-context-final=0 --minimize=false --max-active=7000 --min-active=200 --beam=15.0 --lattice-beam=8.0 --acoustic-scale=1.0 --allow-partial=true --word-symbol-table=/home/aban/devel/aban-kaldi/egs/learn/tdnn/exp/tri7b_tree/graph/words.txt /home/aban/devel/aban-kaldi/egs/learn/tdnn/exp/tri8b/final.mdl /home/aban/devel/aban-kaldi/egs/learn/tdnn/exp/tri7b_tree/graph/HCLG.fst ark:$input ark:./trash/lattice.ark
