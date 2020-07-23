035240b4f7fe08370bab67829b0b78a0ad4806ec
Ban <sawpara@126.com>
2020-07-23 11:53:23 +0800

    save my finger

---
+++ egs/learn/tdnn/init-some-tdnns-for-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+###############################################################################
+# file name    : init-a-tdnn.sh
+# authors      : Ban Zhihua(2018-2020)
+# contact      : sawpara@126.com
+# created time : Wed 08 Jul 2020 11:17:05 AM CST
+###############################################################################
+
+. path.sh
+
+for f in tdnn-models/{0..1}.sh; do
+    if [ -f $f ]; then
+        $f 
+    fi
+done
---
+++ egs/learn/tdnn/src/tdnn-init/tdnn-init.cpp
@@ -0,0 +1,11 @@
+/******************************************************************************
+  > file name    : tdnn-init.cpp
+  > author       : Ban Zhihua
+  > contact      : sawpara@126.com
+  > created time : Thu 23 Jul 2020 11:21:21 AM CST
+******************************************************************************/
+#include <iostream>
+using namespace std;
+int main(){
+    return 0;
+}
