58bd7d049325ccc0c25a501cdde62df7f4c75477
Ban <sawpara@126.com>
2020-07-23 16:39:39 +0800

    save my finger

---
+++ egs/learn/tdnn/src/fake-chain-mb/fake-chain-mb.cpp
@@ -0,0 +1,89 @@
+/******************************************************************************
+  > file name    : fake-chain-mb.cpp
+  > author       : Ban Zhihua
+  > contact      : sawpara@126.com
+  > created time : Thu 23 Jul 2020 03:53:27 PM CST
+******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "base/kaldi-common.h"
+#include "util/common-utils.h"
+#include "nnet3/nnet-chain-training.h"
+#include "cudamatrix/cu-allocator.h"
+#include "nnet3/nnet-chain-example.h"
+
+using namespace kaldi;
+using namespace kaldi::nnet3;
+using namespace kaldi::chain;
+
+int main(int argc, char** argv){
+
+        const char *usage ="usage";
+
+    int32 srand_seed = 0;
+    bool binary_write = true;
+    std::string use_gpu = "yes";
+    NnetChainTrainingOptions opts;
+
+
+    ParseOptions po(usage);
+    po.Register("srand", &srand_seed, "Seed for random number generator ");
+    po.Register("binary", &binary_write, "Write output in binary mode");
+    po.Register("use-gpu", &use_gpu,
+            "yes|no|optional|wait, only has effect if compiled with CUDA");
+
+    opts.Register(&po);
+    #if HAVE_CUDA==1
+    CuDevice::RegisterDeviceOptions(&po);
+    #endif
+    RegisterCuAllocatorOptions(&po);
+
+    po.Read(argc, argv);
+
+
+    string den_fst_rxfilename = "../../exp/tri8b/den.fst";
+    fst::StdVectorFst den_fst;
+    ReadFstKaldi(den_fst_rxfilename, &den_fst);
+
+    string nnet_rxfilename = "../../tdnn-models/0.raw";
+    Nnet nnet;
+    ReadKaldiObject(nnet_rxfilename, &nnet);
+
+    NnetChainTrainer trainer(opts, den_fst, &nnet);
+
+
+    string examples_rspecifier = "ark:../../trash/debug.mb.chain.egs"; 
+    string examples_wspecifier = "ark,t:../../trash/fake.debug.mb.chain.egs";
+    kaldi::nnet3::SequentialNnetChainExampleReader example_reader(examples_rspecifier);
+    kaldi::nnet3::NnetChainExampleWriter example_writer(examples_wspecifier);
+    for (; !example_reader.Done(); example_reader.Next()){
+        auto &eg = example_reader.Value();
+        for (auto &input : eg.inputs){
+            //for (auto &i : input.indexes){
+            //    i.Write(cout, false);
+            //    cout << endl;
+            //}
+            kaldi::Matrix<kaldi::BaseFloat> feats;
+            input.features.GetMatrix(&feats);
+            int min_val = std::numeric_limits<int>::max();
+            for (auto &i : input.indexes){
+                if (min_val > i.t){
+                    min_val = i.t;
+                }
+            }
+            int k = 0;
+            for (auto &i : input.indexes){
+                kaldi::BaseFloat val = i.t-min_val;
+                feats.Row(k).Set(val);
+                k++;
+            }
+            input.features = feats;
+        }
+        trainer.Train(eg);
+
+        example_writer.Write("heihei", eg);
+    }
+    return 0;
+}
---
+++ egs/learn/tdnn/tdnn-compute.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+###############################################################################
+# file name    : debug-train.sh
+# authors      : Ban Zhihua(2018-2020)
+# contact      : sawpara@126.com
+# created time : Tue 09 Jun 2020 04:10:54 PM CST
+###############################################################################
+
+mb_egs=./trash/debug.mb.chain.egs
+
+#if [ ! -f $mb_egs ]; then
+#    nnet3-chain-copy-egs \
+#        --frame-shift=0 \
+#        ark:/home/aban/devel/aban-kaldi/egs/learn/tdnn/exp/tri8b/egs/cegs.2.ark ark:- |\
+#        nnet3-chain-shuffle-egs --buffer-size=5000 --srand=1 ark:- ark:- |\
+#        nnet3-chain-merge-egs --minibatch-size=128,64 ark:- ark:$mb_egs 
+#fi
+
+
+nnet3-chain-subset-egs --randomize-order=false --n=5 ark:./exp/tri8b/egs/cegs.1.ark ark:- |\
+    nnet3-chain-merge-egs --minibatch-size=4 ark:- ark:$mb_egs 
+
+#--read-cache=/home/aban/devel/aban-kaldi/egs/learn/tdnn/exp/tri8b/cache.1 
+#    --computation.debug=false # change to true if you want comands
+    #/home/aban/devel/aban-kaldi/egs/learn/tdnn/exp/tri8b/30.mdl \
+./src/nnet3-chain-train.d.exe \
+    --use-gpu=yes \
+    --apply-deriv-weights=False \
+    --l2-regularize=0.0 \
+    --leaky-hmm-coefficient=0.1 \
+    --xent-regularize=0.1 \
+    --print-interval=10 \
+    --momentum=0.0 \
+    --max-param-change=2.0 \
+    --backstitch-training-scale=0.0 \
+    --backstitch-training-interval=1 \
+    --l2-regularize-factor=0.5 \
+    --optimization.memory-compression-level=2 \
+    --srand=1 \
+    ./tdnn-models/0.raw \
+    /home/aban/devel/aban-kaldi/egs/learn/tdnn/exp/tri8b/den.fst \
+    ark:$mb_egs \
+    ./trash/output.raw 2> chain-train-debug-1.txt
